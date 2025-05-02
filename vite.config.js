638f7a16da8b22c60852202d798be7885cfffa01
Iszha <campomayorisha@gmail.com>
2025-05-03 00:44:15 +0800

    Resolved conflicts and updated feature branch

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
-import tailwindcss from '@tailwindcss/vite'
-
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()
-  ],
-  server: {
-    host: true,       // allow access via local IP (e.g. 192.168.x.x)
-    port: 5173,       // or change this if you want a custom port
-  }
-=======
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
->>>>>>> origin/main
 })
